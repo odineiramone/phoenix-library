8433aff064be44ccbe4681f728e29da000d5ce7c
Odinei Ribeiro <odinei.ribeiro92@gmail.com>
2022-05-26 16:52:45 -0300

    mix format no projeto

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,29 +12,23 @@
 
 alias PhoenixLibrary.Book
 
-Book.create(
-  %{
-    title: "Clube da Luta",
-    publisher: "Editora Leya",
-    cover_photo: "https://i.imgur.com/rhy5L2n.png",
-    authorship: ["Chuck Palahniuk"]
-  }
-)
+Book.create(%{
+  title: "Clube da Luta",
+  publisher: "Editora Leya",
+  cover_photo: "https://i.imgur.com/rhy5L2n.png",
+  authorship: ["Chuck Palahniuk"]
+})
 
-Book.create(
-  %{
-    title: "Mate-me Por Favor",
-    publisher: "LP&M Pocket",
-    cover_photo: "https://i.imgur.com/BW0SKWm.png",
-    authorship: ["Legs McNeil", "Gillian McCain"]
-  }
-)
+Book.create(%{
+  title: "Mate-me Por Favor",
+  publisher: "LP&M Pocket",
+  cover_photo: "https://i.imgur.com/BW0SKWm.png",
+  authorship: ["Legs McNeil", "Gillian McCain"]
+})
 
-Book.create(
-  %{
-    title: "O Guia do Mochileiro das Galáxias",
-    publisher: "Editora Arqueiro",
-    cover_photo: "https://i.imgur.com/0FvAVYu.png",
-    authorship: ["Douglas Adamns"]
-  }
-)
+Book.create(%{
+  title: "O Guia do Mochileiro das Galáxias",
+  publisher: "Editora Arqueiro",
+  cover_photo: "https://i.imgur.com/0FvAVYu.png",
+  authorship: ["Douglas Adamns"]
+})
