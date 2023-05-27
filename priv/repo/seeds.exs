d89390b927ca181e7741b8acbd226fea94b73fa5
Odinei Ribeiro <odinei.ribeiro92@gmail.com>
2023-05-27 02:03:28 -0300

    Atualiza modulo de criacao de livros no arquivo de seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,51 +10,51 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias PhoenixLibrary.Book
+alias PhoenixLibrary.Books
 
-Book.create(%{
+Books.create_book(%{
   title: "Clube da Luta",
   publisher: "Editora Leya",
   cover_photo: "https://i.imgur.com/rhy5L2n.png",
   authorship: ["Chuck Palahniuk"]
 })
 
-Book.create(%{
+Books.create_book(%{
   title: "Mate-me Por Favor",
   publisher: "LP&M Pocket",
   cover_photo: "https://i.imgur.com/BW0SKWm.png",
   authorship: ["Legs McNeil", "Gillian McCain"]
 })
 
-Book.create(%{
+Books.create_book(%{
   title: "O Guia do Mochileiro das Galáxias",
   publisher: "Editora Arqueiro",
   cover_photo: "https://i.imgur.com/0FvAVYu.png",
   authorship: ["Douglas Adamns"]
 })
 
-Book.create(%{
+Books.create_book(%{
   title: "O Senhor das Moscas",
   publisher: "Alfaguara",
   cover_photo: "",
   authorship: ["William Golding"]
 })
 
-Book.create(%{
+Books.create_book(%{
   title: "O Senhor dos Anéis: A Sociedade do Anel",
   publisher: "Martins Fontes",
   cover_photo: "",
   authorship: ["J. R. R. Tolkien"]
 })
 
-Book.create(%{
+Books.create_book(%{
   title: "O Hobbit",
   publisher: "Martins Fontes",
   cover_photo: "",
   authorship: ["J. R. R. Tolkien"]
 })
 
-Book.create(%{
+Books.create_book(%{
   title: "1984",
   publisher: "Companhia das Letras",
   cover_photo: "",
